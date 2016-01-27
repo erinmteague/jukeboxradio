eca4ab806beb06f898e86c335900c8297adf0ad1
Erin <eteague27@gmail.com>
2016-01-27 11:31:47 -0500

    audio dial works

--- radio.js
+++ radio.js
@@ -6,7 +6,6 @@
 	playlist: ["https://ia801405.us.archive.org/6/items/OTRR_Crime_Classics_Singles/cric_53-07-06_ep04_The_Shrapnelled_Body_Of_Charles_Drew_Senior.mp3", "https://ia801405.us.archive.org/6/items/OTRR_Crime_Classics_Singles/cric_53-09-30_ep14_The_Bloody_Bloody_Banks_Of_Fall_River.mp3", "https://ia801405.us.archive.org/6/items/OTRR_Crime_Classics_Singles/cric_53-10-07_ep15_The_Hangman_and_William_Palmer_-_Who_Won.mp3", "https://ia801405.us.archive.org/6/items/OTRR_Crime_Classics_Singles/cric_54-06-30_ep51_Good_Evening_My_Name_Is_Jack_The_Ripper.mp3", "https://ia801405.us.archive.org/6/items/OTRR_Crime_Classics_Singles/cric_54-05-12_ep45_Mr_Clarkes_Skeleton_In_Mr_Arams_Closet.mp3"],
 
 	play: function(){
-		document.getElementById("player").setAttribute('src', this.playlist[this.currentNum]),
 		document.getElementById("player").play();
 	},
 
@@ -27,4 +26,3 @@
 	},
 
 }
-
--- script.js
+++ script.js
@@ -16,4 +16,6 @@ $(document).ready(function(){
 		});
 		$("#onair").removeClass("light");
 	});
+
+	
 });
